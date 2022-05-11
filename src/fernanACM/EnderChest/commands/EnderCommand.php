db99e1b53be1fd65af5222a660f4cf84d4d09417
fernanACM <83558341+fernanACM@users.noreply.github.com>
2022-05-10 21:42:15 -0500

    New version 1.0.1: Fixed permission
    New lib: Commando

--- src/fernanACM/EnderChest/commands/EnderCommand.php
+++ src/fernanACM/EnderChest/commands/EnderCommand.php
@@ -8,54 +8,39 @@ use pocketmine\plugin\Plugin;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\plugin\PluginOwned;
-# Libs - InvMenu
+# Lib - InvMenu
 use muqsit\invmenu\transaction\InvMenuTransactionResult;
 use muqsit\invmenu\transaction\InvMenuTransaction;
 use muqsit\invmenu\type\InvMenuTypeIds;
 use muqsit\invmenu\InvMenu;
+# Lib - Commando
+use CortexPE\Commando\BaseCommand;
 
 use fernanACM\EnderChest\Ender;
 use fernanACM\EnderChest\utils\PluginUtils;
 
-class EnderCommand extends Command implements PluginOwned{
+class EnderCommand extends BaseCommand{
     
-    private $plugin;
-
-    public function __construct(Ender $plugin){
-        $this->plugin = $plugin;
-        
-        parent::__construct("enderchest", "Open EnderChest by fernanACM", "§cUse: /enderchest | ec", ["ender", "ec"]);
+    protected function prepare(): void{
         $this->setPermission("enderchest.acm");
     }
 
-    public function execute(CommandSender $sender, string $commandLabel, array $args){
-        if(count($args) == 0){
-            if($sender instanceof Player) {
-                   $menu = InvMenu::create(InvMenuTypeIds::TYPE_CHEST);
-                   $menu->setName($this->plugin->config->get("Settings")["EnderChest-name"]);
-                   $inv = $menu->getInventory();
-                   $inv->setContents($sender->getEnderInventory()->getContents());
-                   $menu->setListener(function (InvMenuTransaction $transaction) use ($sender): InvMenuTransactionResult{
-                   $sender->getEnderInventory()->setItem($transaction->getAction()->getSlot(), $transaction->getIn());
-                   return $transaction->continue();
-                   });
-                   $menu->send($sender);
-                   if($this->plugin->config->get("Settings")["EnderChest-no-sound"]){
-                      PluginUtils::PlaySound($sender, $this->plugin->config->get("Settings")["EnderChest-sound"], 50, 1);   
-                   }
-            } else {
-                $sender->sendMessage("Use this command in-game");
-            }
+    public function onRun(CommandSender $sender, string $aliasUsed, array $args): void{
+        if (!$sender instanceof Player) {
+              $sender->sendMessage("Use this command in-game");
+               return;
         }
-        return true;
-    }
-    
-    public function getPlugin(): Plugin{
-        return $this->plugin;
-    }
-
-    public function getOwningPlugin(): Ender{
-        return $this->plugin;
+        $menu = InvMenu::create(InvMenuTypeIds::TYPE_CHEST);
+        $menu->setName(Ender::getInstance()->config->get("Settings")["EnderChest-name"]);
+        $inv = $menu->getInventory();
+        $inv->setContents($sender->getEnderInventory()->getContents());
+        $menu->setListener(function (InvMenuTransaction $transaction) use ($sender): InvMenuTransactionResult{
+        $sender->getEnderInventory()->setItem($transaction->getAction()->getSlot(), $transaction->getIn());
+        return $transaction->continue();
+        });
+        $menu->send($sender);
+        if(Ender::getInstance()->config->get("Settings")["EnderChest-no-sound"]){
+            PluginUtils::PlaySound($sender, Ender::getInstance()->config->get("Settings")["EnderChest-sound"], 50, 1);   
+        }
     }
 }
